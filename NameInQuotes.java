2fdac1019fe9bc5cb7ae2e6d80cd29700f5012da
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-14 23:40:03 +0530

    Add files via upload

---
+++ NameInQuotes.java
@@ -0,0 +1,15 @@
+class NameInQuotes {
+    public static void main(String[] args) {
+         System.out.println("'Rishabh Raj'");
+         System.out.println("\"Rishabh Raj\"");
+
+         System.out.println("'");
+         System.out.println('"');
+
+         //System.out.println("""); //CE
+         //System.out.println('''); //CE
+  
+         System.out.println("\"");
+         System.out.println('\'');
+    }
+}
